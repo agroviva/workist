030c05755319710ac59c5f9712c88940fc5946ab
Neo Morina <neomorina@gmail.com>
2018-12-03 14:13:19 +0100

    Update Validation.php

--- Helpful/Validation.php
+++ Helpful/Validation.php
@@ -1,12 +1,21 @@
 <?php
 namespace Saaora\Helpful;
+use Illuminate\Support\Str;
 
 class Validation
 {
 	const WORKSPACE_REGEX = '/^[\w-]+$/';
 
+	/**
+	 * Check if the given workspace name is valid
+	 * @param  string  $workspace
+	 * @return boolean
+	 */
 	public static function isWorkspaceValid(string $workspace)
 	{
+		if (Str::length($workspace) > 60) {
+			return false;
+		}
 		return preg_match(self::WORKSPACE_REGEX, $workspace);
 	}
 }
