82c672980b4f3cdb763f0521b47c12bc9db5f5f3
Enver Morinaj <emorinaj@agroviva.de>
2018-12-03 14:33:07 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- Database/Logins.php
+++ Database/Logins.php
@@ -1,21 +1,21 @@
 <?php
+
 namespace Saaora\Database;
+
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Capsule\Manager as Capsule;
 
 class Logins
 {
-    const TABLE = "logins";
+    const TABLE = 'logins';
 
-    static function boot(){
-    	if (!Schema::hasTable('logins'))
-		{
-		    Schema::create('logins', function($table)
-			{
-			    $table->increments('id')->unique();
-			    $table->string('username', 60)->unique();
-			    $table->string('password', 255);
-			});
-		}
+    public static function boot()
+    {
+        if (!Schema::hasTable('logins')) {
+            Schema::create('logins', function ($table) {
+                $table->increments('id')->unique();
+                $table->string('username', 60)->unique();
+                $table->string('password', 255);
+            });
+        }
     }
 }
